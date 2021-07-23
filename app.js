791dcffc105fa19c45ca459b25cdf617f6d03329
LucioMart <luchoweb15@gmail.com>
2021-07-23 20:43:57 -0300

    Agregar el static public al app. Incorporar estilos al home

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const port = 3030
 
 const app = express()
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
 
 app.get('/about', (req, res) => res.sendFile(path.join(__dirname, 'views', 'about.html')))
